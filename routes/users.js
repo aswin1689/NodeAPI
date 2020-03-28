303441aa3b0bd6c57495755d147f9c04e5ed76ba
Aswin Kumar Alapati <aswin_k_alapati@homedepot.com>
2020-03-27 22:02:43 -0400

    update npm packages

--- routes/users.js
+++ routes/users.js
@@ -1,53 +1,53 @@
-const express = require("express");
-const router = require("express-promise-router")();
+const express = require('express');
+const router = require('express-promise-router')();
 
-const UsersController = require("../controllers/users");
+const UsersController = require('../controllers/users');
 
 const {
-  validateBody,
-  validateParam,
-  schemas
-} = require("../helpers/routeHelpers");
+	validateBody,
+	validateParam,
+	schemas
+} = require('../helpers/routeHelpers');
 
 router
-  .route("/")
-  .get(UsersController.index)
-  .post(validateBody(schemas.userSchema), UsersController.newUser);
+	.route('/')
+	.get(UsersController.index)
+	.post(validateBody(schemas.userSchema), UsersController.newUser);
 
 router
-  .route("/:userId")
-  .get(validateParam(schemas.idSchema, "userId"), UsersController.getUser)
-  .put(
-    [
-      validateParam(schemas.idSchema, "userId"),
-      validateBody(schemas.userSchema)
-    ],
-    UsersController.replaceUser
-  )
-  .patch(
-    [
-      validateParam(schemas.idSchema, "userId"),
-      validateBody(schemas.userOptionalSchema)
-    ],
-    UsersController.updateUser
-  )
-  .delete(
-    validateParam(schemas.idSchema, "userId"),
-    UsersController.deleteUser
-  );
+	.route('/:userId')
+	.get(validateParam(schemas.idSchema, 'userId'), UsersController.getUser)
+	.put(
+		[
+			validateParam(schemas.idSchema, 'userId'),
+			validateBody(schemas.userSchema)
+		],
+		UsersController.replaceUser
+	)
+	.patch(
+		[
+			validateParam(schemas.idSchema, 'userId'),
+			validateBody(schemas.userOptionalSchema)
+		],
+		UsersController.updateUser
+	)
+	.delete(
+		validateParam(schemas.idSchema, 'userId'),
+		UsersController.deleteUser
+	);
 
 router
-  .route("/:userId/products")
-  .get(
-    validateParam(schemas.idSchema, "userId"),
-    UsersController.getUserProducts
-  )
-  .post(
-    [
-      validateParam(schemas.idSchema, "userId"),
-      validateBody(schemas.productSchema)
-    ],
-    UsersController.newUserProduct
-  );
+	.route('/:userId/products')
+	.get(
+		validateParam(schemas.idSchema, 'userId'),
+		UsersController.getUserProducts
+	)
+	.post(
+		[
+			validateParam(schemas.idSchema, 'userId'),
+			validateBody(schemas.productSchema)
+		],
+		UsersController.newUserProduct
+	);
 
 module.exports = router;
