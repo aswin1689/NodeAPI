c1f5a29a5edb3714a564c07770a03ce1bd1117ef
Aswin Kumar Alapati <aswin1689@gmail.com>
2020-03-29 20:07:23 -0400

    allow CORS

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ const users = require('./routes/users');
 app.use(logger('dev'));
 app.use(express.json());
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
 //routes
 app.use('/api', users);
 
